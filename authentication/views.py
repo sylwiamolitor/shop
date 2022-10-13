29685560390e698ab25ef16b415cc19fd2d76b8d
Sylwia <sylwiamlr@gmail.com>
2022-10-13 13:24:59 +0200

    Logging

---
+++ authentication/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+
+
+def log_in(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+
+            user = authenticate(
+                request,
+                username=form.cleaned_data.get('username'),
+                password=form.cleaned_data.get('password')
+            )
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                context = {'form': form}
+                return render(request, 'authentication/login.html', context)
+        else:
+            context = {'form': form}
+            return render(request, 'authentication/login.html', context)
+
+    else:
+        context = {'form': LoginForm()}
+        return render(request, 'authentication/login.html', context)
+
+
+def log_out(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect('index')
---
+++ home/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import index
+
+urlpatterns = [
+    path('', index, name='index'),
+    #path('admin/', admin.site.urls),
+    path('', include('authentication.urls')),
+]
